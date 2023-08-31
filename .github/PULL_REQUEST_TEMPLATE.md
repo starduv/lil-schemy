88da2b8bf6ee13dc66d262f3c23d7a7223d47750
Joel Rainear Wills <joel.rainear-wills@mastery.net>
2023-08-31 14:03:35 -0500

    16-support-multipart-form-data (#17)
    * modify media type for request body and response
    
    * add pr template
    
    ---------
    
    Co-authored-by: Joel Rainear-Wills <joel.raineaer-wills@mastery.net>

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,45 @@
+## Issue Number: [#1]
+
+## PR Checklist
+
+Please check if your PR fulfills the following requirements:
+
+- [ ] Tests for the changes have been added (for bug fixes / features)
+- [ ] Build (`yarn build`) was run locally and any changes were pushed
+
+---
+
+<!-- Please try to limit your pull request to one type, submit multiple pull requests if needed. -->
+
+Please check the type of change your PR introduces:
+
+- [ ] Bugfix
+- [ ] Enhancement
+- [ ] Other (please describe):
+
+---
+
+## What is the current behavior?
+
+<!-- Please describe the current behavior that you are modifying, or link to a relevant issue. -->
+
+-
+-
+
+## What is the new behavior?
+
+<!-- Please describe the behavior or changes that are being added by this PR. -->
+
+-
+-
+
+## Does this introduce a breaking change?
+
+- [ ] Yes (please describe):
+- [ ] No
+
+---
+
+## Other information
+
+<!-- Any other information that is important to this PR such as screenshots of how the component looks before and after the change. -->
--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,25 @@ export { getRootFiles } from './utils';
 
 type NumberFormat = "int32" | "int64" | "float" | "double";
 type StringFormat = "date-time" |
-"time" |
-"date" |
-"duration" |
-"email" |
-"idn-email" |
-"hostname" |
-"idn-hostname" |
-"ipv4" |
-"ipv6" |
-"uuid" |
-"uri" |
-"uri-reference" |
-"uri-template" |
-"json-pointer" |
-"relative-json-pointer" |
-"regex" |
-"iri" |
-"iri-reference" |
-undefined;
+    "time" |
+    "date" |
+    "duration" |
+    "email" |
+    "idn-email" |
+    "hostname" |
+    "idn-hostname" |
+    "ipv4" |
+    "ipv6" |
+    "uuid" |
+    "uri" |
+    "uri-reference" |
+    "uri-template" |
+    "json-pointer" |
+    "relative-json-pointer" |
+    "regex" |
+    "iri" |
+    "iri-reference" |
+    undefined;
 
 export type format = StringFormat | NumberFormat | undefined;
 
@@ -40,12 +40,13 @@ export interface ResponseOptions {
     description: string;
     example?: string;
     statusCode: number;
+    mediaType?: string;
 }
 export function LilResponse<ResponseType>(response: ResponseType, options: ResponseOptions) {
     return response;
 }
 
-export type LilBodyParam<Param, Required extends boolean = true> = Param;
+export type LilBodyParam<Param, Required extends boolean = true, MediaType extends string = "application/json"> = Param;
 export type LilHeader<Param, Required extends boolean = true, Format extends format = undefined> = Param;
 export type LilQueryParam<Param, Required extends boolean = false, Format extends format = undefined> = Param;
 export type LilRouteParam<Param, Required extends true = true, Format extends format = undefined> = Param;
